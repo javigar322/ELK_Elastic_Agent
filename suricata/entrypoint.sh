0dada2519b096bc45f6ebf201c9f0e93a0cdc761
ricardo <rferguz3003@g.educaand.es>
2024-12-18 17:37:18 +0100

    entrypoint.sh cambiado

--- suricata/entrypoint.sh
+++ suricata/entrypoint.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-# Detectar la interfaz de red activa (excluyendo `lo`)
-INTERFACE=$(ip -o link show | awk -F': ' '{print $2}' | grep -v lo | head -n 1)
-
-if [ -z "$INTERFACE" ]; then
-    echo "No active network interface found!"
+# Verificar si la variable de entorno NGINX_INTERFACE está configurada
+if [ -z "$NGINX_INTERFACE" ]; then
+    echo "No se pudo detectar la interfaz de red. Verifica la configuración de la variable de entorno NGINX_INTERFACE."
     exit 1
 fi
 
 # Ajustar el MTU de la interfaz detectada
-echo "Deberia de funcionar este. Adjusting MTU for $INTERFACE to 1500"
-ip link set $INTERFACE mtu 1500
+echo "Adjusting MTU for $NGINX_INTERFACE to 1500"
+ip link set $NGINX_INTERFACE mtu 1500
 
 # Actualizar reglas de Suricata si está habilitado
 if [ "$SURICATA_UPDATE_ENABLED" = "true" ]; then
@@ -26,6 +24,9 @@ if [ -f /etc/suricata/rules/custom.rules ]; then
     cat /etc/suricata/rules/custom.rules >> /var/lib/suricata/rules/suricata.rules
 fi
 
+# Reemplazar la interfaz en el archivo de configuración de Suricata
+sed -i "s/INTERFACE/$NGINX_INTERFACE/g" /etc/suricata/suricata.yaml
+
 # Iniciar Suricata con la interfaz detectada
-echo "Starting Suricata on interface $INTERFACE..."
-exec suricata -c /etc/suricata/suricata.yaml -i $INTERFACE -vv
+echo "Starting Suricata on interface $NGINX_INTERFACE..."
+exec suricata -c /etc/suricata/suricata.yaml -i $NGINX_INTERFACE -vv
